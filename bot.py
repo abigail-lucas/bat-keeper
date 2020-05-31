fb913ee5d4424df686924fa6ce9d26556dc6fd65
abigail <abigailwolfmoon@gmail.com>
2020-05-31 14:00:00 +0200

    Finished the three commands

--- bot.py
+++ bot.py
@@ -65,21 +65,37 @@ class PickMe(discord.Client):
                     response = "There aren't enough users to pick from!"
                 else:
                     picked_users = []
-                    for i in range(amount):
+                    i = 0
+                    while i < amount:
                         option = pool[random.randint(0, len(pool)-1)]
                         if option not in picked_users:
                             picked_users.append(option)
+                            i += 1
                     for picked in picked_users:
-                        picked_name = picked.nick if picked.nick != None else picked.name
-                        response += f"I have picked {picked_name}\n"
+                        response += f"I have picked <@{picked.id}>\n"
             elif re.match(self.command_amount_reg, message_content):
                 a_s = message_content.find("{") + 1
                 a_e = message_content.find("}")
-                amount = message_content[a_s:a_e]
-                response = f"You have chosen {amount} people"
+                amount = int(message_content[a_s:a_e])
                 pool = visible_members
+                if(amount > 5):
+                    response = "You can't pick more than 5 random users"
+                elif(len(pool) < amount):
+                    response = "There aren't enough users to pick from!"
+                else:
+                    picked_users = []
+                    i = 0
+                    while i < amount:
+                        option = pool[random.randint(0, len(pool)-1)]
+                        if option not in picked_users:
+                            picked_users.append(option)
+                            i += 1
+                    for picked in picked_users:
+                        response += f"I have picked <@{picked.id}>\n"
             else:
                 pool = visible_members
+                picked = pool[random.randint(0, len(pool)-1)]
+                response = f"I have picked <@{picked.id}>"
 
             # picked = pool[random.randint(0, len(pool)-1)]
             # response = f"I choose {picked}"
